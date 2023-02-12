372747cc11683c6af8d0ed1ba411441a4b62a2d0
Rodrigo Lopes <rjr89000@gmail.com>
2023-02-12 12:06:36 -0300

    Up projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+# projeto-Caravana-Online-Flamengo
+<br>
+
+**Site autoral, para venda de passagens e ingressos para jogos do Flamengo, consumindo API Football - Rapidapi e integração com Netlify.**
+
+<br>
+  https://caravana-jogos-mengao-srs.netlify.app/
+<br>
+
+*By: Rodrigo Lopes*
---
+++ js/script.js
@@ -0,0 +1,13 @@
+document
+  .getElementById('proximos-jogos')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = 'proximos-jogos.html';
+  });
+
+// document
+//   .getElementById('comprar-passagem')
+//   .addEventListener('click', function (event) {
+//     event.preventDefault();
+//     window.location.href = 'www.google.com';
+//   });
